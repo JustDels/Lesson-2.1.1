1d6e6a0aff87d916ef92d6dca136396a0c6e8bc9
Dmitry Toporkov <dels.axe@gmail.com>
2021-06-10 14:33:08 +0500

    Accrual of miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 550;
+        int rate = 20;
+        int bonus = price / rate;
+        System.out.println("Начислено миль: " + bonus);
+    }
+}
